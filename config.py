4f32c3a2307da1506b45e254da46a1edda9b8f74
nidhaldring <dringthedreamer@gmail.com>
2019-10-04 18:08:36 +0100

    added more configs

--- config.py
+++ config.py
@@ -1,6 +1,11 @@
 
 
+# must be changed accordingly
 
 class CONFIG:
-    DB_NAME = ""
+    
+    DB_USER = "root"
+    DB_PASSWORD = "root"
+    DB_HOST = "localhost"
+    DB_NAME = "test"
     USERS_TABLE_NAME = "users"
