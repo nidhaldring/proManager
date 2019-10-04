dc375a79e99373beacf4cc31a2efffd64c639bc4
nidhaldring <dringthedreamer@gmail.com>
2019-10-04 17:41:07 +0100

    created exceptions for models

---
+++ models/exceptions.py
@@ -0,0 +1,9 @@
+
+class ModelAlreadyInsertedException(Exception):pass
+
+class ModelNotInsertedException(Exception):pass
+
+class ModelUniqueConstraintException(Exception):pass
+
+
+class ModelMissingConstructorArgumentException(Exception):pass
