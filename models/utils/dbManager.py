ff042bc7654328be928f4431485e7c3c421a1beb
nidhaldring <dringthedreamer@gmail.com>
2019-10-04 17:41:43 +0100

    created utils package for models

---
+++ models/utils/dbManager.py
@@ -0,0 +1,33 @@
+
+import pymysql
+
+from config import CONFIG
+
+class DbManager:
+
+	def __init__(self):
+
+		self.connectionSettings = dict(
+								db=CONFIG.DB_NAME,
+								user=CONFIG.DB_USER,
+								password=CONFIG.DB_PASSWORD,
+								host=CONFIG.DB_HOST
+							)
+
+	def execute(self,sql):
+
+		'''
+		executes the given sql
+		returning the result or none if none
+		'''
+
+		conn = pymysql.connect(**self.connectionSettings)
+		cursor = conn.cursor()
+
+		cursor.execute(sql)
+		res = cursor.fetchall()
+
+		conn.commit()
+		conn.close()
+
+		return res
---
+++ models/utils/sqlQueryMaker.py
@@ -0,0 +1,44 @@
+
+
+class SQLQueryMaker:
+	
+	def __init__(self,table):
+		
+		self.table = table
+
+	def makeInsertQuery(self,data:dict) -> str:
+
+		sql = f"insert into {self.table}"
+		sql += "(" + ",".join(data.keys()) +")"
+		sql += " values(" + ",".join([f"'{i}'" for i in data.values()]) + ");"
+
+		return sql
+
+	def makeDeleteQuery(self,cond) -> str:
+		
+		return f"delete from {self.table} where {self._createCond(cond)};"
+
+	def makeUpdateQuery(self,data:dict,cond:dict) -> str:
+		
+		cond = self._createCond(cond)
+		sql = f"update {self.table} set "
+		sql +=  ",".join(["{}='{}'".format(i,j) for i,j in data.items()])
+		sql += " where " + cond + ";"
+
+		return sql
+
+	def makeSearchQuery(self,cond:dict,orderBy=[],limit=None,desc=False) -> str:
+
+		if desc and not orderBy:
+			raise Exception("desc can't be set true when no orderBy list is given")
+
+		limit = f" limit {limit}" if limit else ""
+		order = f" order by " + ",".join(orderBy) if orderBy else ""
+		order += " desc" if desc else ""
+		return f"select * from {self.table} where {self._createCond(cond)}{limit}{order};"
+
+	def _createCond(self,cond:dict):
+		
+		'''returns the conditional part of sql statement - where'''
+		return " and ".join(["{}='{}'".format(i,j) for i,j in cond.items()])
+
