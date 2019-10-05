e75a6185ebff4b9c2054581c38cafd15b589f1bb
nidhaldring <dringthedreamer@gmail.com>
2019-10-05 13:57:33 +0100

    updaed config attrs name

--- config.py
+++ config.py
@@ -3,9 +3,9 @@
 # must be changed accordingly
 
 class CONFIG:
-    
+
     DB_USER = "root"
     DB_PASSWORD = "root"
     DB_HOST = "localhost"
     DB_NAME = "test"
-    USERS_TABLE_NAME = "users"
+    MEMBERS_TABLE_NAME = "users"
