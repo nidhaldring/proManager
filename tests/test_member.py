b56dbe46492d2db56cad9b314d8f6657d5b8c164
nidhaldring <dringthedreamer@gmail.com>
2019-10-05 13:58:12 +0100

    renamed test_user to test_member

--- tests/test_member.py
+++ tests/test_member.py
@@ -2,7 +2,7 @@
 
 import unittest
 
-from models.user import *
+from models.member import *
 
 # mock objects
 class MockDbManager:
@@ -16,27 +16,27 @@ class MockQueryMaker:
 
 #
 
-class TestUser(unittest.TestCase):
+class TestMember(unittest.TestCase):
 
 	def __init__(self,*args,**kargs):
 
 		super().__init__(*args,**kargs)
-		User.dbManager = MockDbManager()
-		User.queryMaker = MockQueryMaker()
-		User.table = "table"
+		Member.dbManager = MockDbManager()
+		Member.queryMaker = MockQueryMaker()
+		Member.table = "table"
 
 	def setUp(self):
-		self.u = User("h","g","h@g.com")
+		self.u = Member("h","g","h@g.com")
 
-	def test_delete_raises_when_deleting_unregistred_user(self):
+	def test_delete_raises_when_deleting_unregistred_member(self):
 		self.assertRaises(ModelNotInsertedException,self.u.delete)
 
 
-	def test_insert_raises_when_reinserting_same_user(self):
+	def test_insert_raises_when_reinserting_same_member(self):
 		self.u.insert()
 		self.assertRaises(ModelAlreadyInsertedException,self.u.insert)
 
-	def test_register_set_user_id(self):
+	def test_register_set_member_id(self):
 		self.u.insert()
 		self.assertNotEqual(self.u.id,None)
 
@@ -45,21 +45,21 @@ class TestUser(unittest.TestCase):
 		self.u.delete()
 		self.assertEqual(self.u.id,None)
 
-	def test_update_raises_when_updating_unregistred_user(self):
+	def test_update_raises_when_updating_unregistred_member(self):
 		newData = {"email":"ff@ff.com","password":"555"}
 		self.assertRaises(ModelNotInsertedException,self.u.update,newData)
 
-	def test_update_change_user_attributes(self):
+	def test_update_change_member_attributes(self):
 		newData = {"email":"ff@ff.com","password":"555"}
 		self.u.insert().update(newData)
 		for key,v in newData.items():
 			self.assertEqual(getattr(self.u,key),v)
 
-	def test_user_id_raises_when_attempted_to_be_changed(self):
+	def test_member_id_raises_when_attempted_to_be_changed(self):
 		with self.assertRaises(AttributeError):
 			self.u.id = 0
 
-	def test_query_returns_user_with_correct_condition(self):
-		u = User.query({"email":"1"})
+	def test_query_returns_Member_with_correct_condition(self):
+		u = Member.query({"email":"1"})
 		self.assertNotEqual(u,[])
 		self.assertEqual(u[0].email,"1")
