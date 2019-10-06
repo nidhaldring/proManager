141bb531a518489a8ba29cd5a8e4ad94a22849c1
nidhaldring <dringthedreamer@gmail.com>
2019-10-06 09:14:45 +0100

    added tests for constructor

--- tests/test_member.py
+++ tests/test_member.py
@@ -7,7 +7,7 @@ from models.member import *
 # mock objects
 class MockDbManager:
 	def execute(self,sql):
-		return [("1","1","1","1")]
+		return [("1","1","1","1","1")]
 class MockQueryMaker:
 	def makeInsertQuery(*args,**kargs):pass
 	def makeDeleteQuery(*args,**kargs):pass
@@ -18,15 +18,11 @@ class MockQueryMaker:
 
 class TestMember(unittest.TestCase):
 
-	def __init__(self,*args,**kargs):
-
-		super().__init__(*args,**kargs)
+	def setUp(self):
 		Member.dbManager = MockDbManager()
 		Member.queryMaker = MockQueryMaker()
 		Member.table = "table"
-
-	def setUp(self):
-		self.u = Member("h","g","h@g.com")
+		self.u = Member("h","g","h@g.com","hhh")
 
 	def test_delete_raises_when_deleting_unregistred_member(self):
 		self.assertRaises(ModelNotInsertedException,self.u.delete)
@@ -46,11 +42,11 @@ class TestMember(unittest.TestCase):
 		self.assertEqual(self.u.id,None)
 
 	def test_update_raises_when_updating_unregistred_member(self):
-		newData = {"email":"ff@ff.com","password":"555"}
+		newData = {"phone":"ff@ff.com","surname":"555"}
 		self.assertRaises(ModelNotInsertedException,self.u.update,newData)
 
 	def test_update_change_member_attributes(self):
-		newData = {"email":"ff@ff.com","password":"555"}
+		newData = {"phone":"ff@ff.com","surname":"555"}
 		self.u.insert().update(newData)
 		for key,v in newData.items():
 			self.assertEqual(getattr(self.u,key),v)
@@ -60,6 +56,21 @@ class TestMember(unittest.TestCase):
 			self.u.id = 0
 
 	def test_query_returns_Member_with_correct_condition(self):
-		u = Member.query({"email":"1"})
+		u = Member.query({"phone":"1"})
 		self.assertNotEqual(u,[])
-		self.assertEqual(u[0].email,"1")
+		self.assertEqual(u[0].phone,"1")
+
+	def test_member_constructor_raises_when_missing_dbManager_attrs(self):
+		Member.dbManager = None
+		with self.assertRaises(ModelMissingAttributeException):
+			u = Member("1","1","2","3")
+
+	def test_member_constructor_raises_when_missing_table_attrs(self):
+		Member.table = None
+		with self.assertRaises(ModelMissingAttributeException):
+			u = Member("1","1","2","3")
+
+	def test_member_constructor_raises_when_missing_queryMaker_attrs(self):
+		Member.queryMaker = None
+		with self.assertRaises(ModelMissingAttributeException):
+			u = Member("1","1","2","3")
